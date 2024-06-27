e73b27a0e396283ab767fa9dc4891c0837a88aad
sundarisankar <168564705+sundarisankar@users.noreply.github.com>
2024-06-27 13:45:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+function updateClock() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    const secondDegrees = (seconds / 60) * 360 + 90;
+    const minuteDegrees = (minutes / 60) * 360 + 90;
+    const hourDegrees = (hours / 12) * 360 + 90;
+
+    const secondHand = document.querySelector('.second-hand');
+    const minuteHand = document.querySelector('.minute-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
